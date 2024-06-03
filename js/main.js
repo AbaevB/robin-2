d79f2af428b9540012f43fced6f4150be0048378
AbaevB <abaev70@hotmail.com>
2024-06-04 02:15:32 +0700

    feat(main.js) add scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+  $('.header__link').on('click', function(event){
+    event.preventDefault();
+
+    let href = $(this).attr('href');
+    let offset = $(href).offset().top;
+
+    $('body,html').animate({
+      scrollTop: offset,
+    }, 700, function(){
+      setTimeout(function(){
+        window.location.href = href;
+      }, 700); // Задержка в 200 миллисекунд перед переходом по ссылке
+    });
+  });
+});
+
 function readMore(){
   alert('Функция Readmore еще не написана!');
 }
