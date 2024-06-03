15cc6c3cf2533c5a3e435ad813c68917215fc6c6
AbaevB <abaev70@hotmail.com>
2024-06-04 02:57:45 +0700

    feat(main.js): add scroll to top button

--- js/main.js
+++ js/main.js
@@ -15,6 +15,26 @@ $(document).ready(function(){
   });
 });
 
+
+///scroll to top
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("toTop").style.display = "block";
+    } else {
+        document.getElementById("toTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 function readMore(){
   alert('Функция Readmore еще не написана!');
 }
