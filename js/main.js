a489b482d3e62ca731ca00da52c1ae4d2b98c982
AbaevB <abaev70@hotmail.com>
2024-06-08 01:31:06 +0700

    feat(section work): section work layout
    - section woprk desktop layout complete
    - topFunction function rewritten in jQuery
    
    # work complete

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     }, 700, function(){
       setTimeout(function(){
         window.location.href = href;
-      }, 700); // Задержка в 200 миллисекунд перед переходом по ссылке
+      }, 1000); // Задержка в 200 миллисекунд перед переходом по ссылке
     });
   });
 });
@@ -18,22 +18,17 @@ $(document).ready(function(){
 
 ///scroll to top
 
-window.onscroll = function() {scrollFunction()};
+$(window).on('scroll', function() {
+  if ($(document).scrollTop() > 20) {
+      $('#toTop').fadeIn();
+  } else {
+      $('#toTop').fadeOut();
+  }
+});
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("toTop").style.display = "block";
-    } else {
-        document.getElementById("toTop").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$('#toTop').on('click', function() {
+  $('html, body').animate({scrollTop: 0}, 800);
+});
 
 function readMore(){
   alert('Функция Readmore еще не написана!');
